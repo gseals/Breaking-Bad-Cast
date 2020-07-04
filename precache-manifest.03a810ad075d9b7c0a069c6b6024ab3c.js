ef817e94ac5ca28d7a5ff1caefc74d405d534d5d
Gabriel Seals <sealsgj@gmail.com>
2020-07-04 18:44:41 -0500

    Updates

---
+++ precache-manifest.03a810ad075d9b7c0a069c6b6024ab3c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2526136b64a938ffabe2cb426f2fccf9",
+    "url": "/Breaking-Bad-Cast/index.html"
+  },
+  {
+    "revision": "e67a998e2040df02267c",
+    "url": "/Breaking-Bad-Cast/static/css/main.b78acc23.chunk.css"
+  },
+  {
+    "revision": "063a7d9a38da3de85d6e",
+    "url": "/Breaking-Bad-Cast/static/js/2.813b259a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Breaking-Bad-Cast/static/js/2.813b259a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67a998e2040df02267c",
+    "url": "/Breaking-Bad-Cast/static/js/main.e04c592e.chunk.js"
+  },
+  {
+    "revision": "a3b6a786245ad3955008",
+    "url": "/Breaking-Bad-Cast/static/js/runtime-main.8015c9c4.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/Breaking-Bad-Cast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/Breaking-Bad-Cast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/Breaking-Bad-Cast/static/media/spinner.235b9b3e.gif"
+  }
+]);
